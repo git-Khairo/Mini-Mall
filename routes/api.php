f286ad05c58df07a96fe775bbf4ca5f8f044ed31
mariosaaoub@gmail.com <mariosaaoub207@gmail.com>
2024-12-04 16:20:06 +0300

    add more things

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// public Route
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+Route::get('/categories',[CategoryController::class,'index']);
+Route::get('/categories/{id}',[CategoryController::class,'show']);
+Route::get('/products',[ProductController::class,'index']);
+Route::get('/products/{id}',[ProductController::class,'show']);
+
+//pro Rote
+Route::group(['middleware'=>['auth:sanctum']], function (){
+    Route::post('/logout',[AuthController::class,'logout']);
+});
