8deb2d788cd18bb97efd8a41a7e208c41f2663af
Mhd Khair <mhd.khalifeh2004@gmail.com>
2024-12-20 00:30:42 +0300

    Final phone

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
     public  function login(Request $request){
 
         $data=$request->validate([
-            'email'=>'required|email|exists:users',
+            'email'=>'required|',
             'password'=>'required',
         ]);
 
